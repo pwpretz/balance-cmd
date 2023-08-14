14e9bd6e6ba6a8541a173bb26ec4a53fcc1c968a
pwpretz <142225169+pwpretz@users.noreply.github.com>
2023-08-14 17:03:40 +0500

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,7 +1,11 @@
+else if (msg.includes("bal")) bal()
+
+function bal() {
 bot.chat("/bal")
       bot.on("messagestr", async (message) => {
         msg = await message
         if (msg.split(' ')[0] === "Balance:") {
           bot.chat("/m <name> Balance: " + msg.split(' ')[1])
         }
-})
+})    
+}
